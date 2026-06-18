9dd708cbaab9c0bb47bc63a69272b238db67e634
tojointhesociety <klesevvlad@gmail.com>
2026-06-18 18:36:03 +0300

    каталог книг

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path(
+        "", views.BookListView.as_view(), name="book_list"
+    ),  # главная теперь = список книг
+    path("book/<int:pk>/", views.BookDetailView.as_view(), name="book_detail"),
     path("librarian/", views.librarian_page, name="librarian_page"),
     path("guest/", views.guest_page, name="guest_page"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from .models import Book, BookCopy
 from .decorators import librarian_required, guest_required
 
 
 def home(request):
     return render(request, "home.html")
+
+
+class BookListView(ListView):
+    model = Book
+    template_name = "book_list.html"
+    context_object_name = "books"
+    paginate_by = 10
+
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = "book_detail.html"
+    context_object_name = "book"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        copies = self.object.copies.all()
+
+        if not self.request.user.groups.filter(name="Librarians").exists():
+            copies = copies.filter(status="available")
+        context["copies"] = copies
+        return context
 
 
 @librarian_required
